91f48989f0a159358c2a4a9dffd57c50fa800cfb
Hayato Kawakita <hkawakit@c1r3s6.42tokyo.jp>
2021-09-24 17:09:18 +0900

    setup

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,12 +6,12 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 16:55:57 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/09/24 17:05:40 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/09/24 17:09:07 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#ifndef PIPEX_H
+# define PIPEX_H
 
 # include <unistd.h>
 # include <fcntl.h>
