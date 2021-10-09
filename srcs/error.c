6c02df54344eeb48d288836466a5aff963f5c569
toruneko1129 <kawakita.hayato@gmail.com>
2021-10-10 02:34:05 +0900

    updated

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 18:03:41 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/09/25 01:24:37 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/10/10 02:33:38 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,14 @@ void	execve_error_exit(char **cmdarray, char *pathname)
 		exit_status = CMD_NOT_FOUND;
 	else if (access(pathname, X_OK) == -1)
 		exit_status = PERMISSION_DENIED;
+	ft_putstr_fd(BASH, STDERR);
 	if (exit_status == CMD_NOT_FOUND)
 	{
 		ft_putstr_fd(cmdarray[0], STDERR);
 		ft_putendl_fd(CMD_NOT_FOUND_MSG, STDERR);
 	}
 	else
-	{
-		ft_putstr_fd(BASH, STDERR);
 		perror(pathname);
-	}
 	free_2darray(cmdarray);
 	free(pathname);
 	exit(exit_status);
