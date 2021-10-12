49d38f83dce23b9d1c9b3c080b882ed93abf42ec
Hayato Kawakita <hkawakit@c1r3s6.42tokyo.jp>
2021-10-12 13:10:29 +0900

    add test

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 make
 ./pipex || echo $?
+./pipex 1 cat cat outfile && echo $?
 ./pipex 1 2 3 outfile || echo $?
 ./pipex infile hoge fuga outfile || echo $?
 ./pipex infile "   " "" outfile || echo $?
 ./pipex infile "/hoge" "/fuga" outfile || echo $?
 ./pipex infile "hoge -e -e -e -e" "fuga fuga" outfile || echo $?
+./pipex infile "/bin/cat" "/bin/cat -e" outfile && cat outfile && echo $?
+./pipex infile "cat" "cat -e" outfile && cat outfile && echo $?
