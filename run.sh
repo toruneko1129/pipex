5fc2e1309914594c887db1448cf0b02a60a4de69
toruneko1129 <kawakita.hayato@gmail.com>
2021-10-12 04:47:45 +0200

    add tester

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 make
 ./pipex || echo $?
 ./pipex 1 2 3 outfile || echo $?
+./pipex infile hoge fuga outfile || echo $?
+./pipex infile "   " "" outfile || echo $?
+./pipex infile "/hoge" "/fuga" outfile || echo $?
+./pipex infile "hoge -e -e -e -e" "fuga fuga" outfile || echo $?
