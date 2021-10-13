e1268223c86659268ec0847010134539f1ac919e
toruneko1129 <kawakita.hayato@gmail.com>
2021-10-13 17:51:05 +0200

    correspond unset path

--- bnssrcs/command_bonus.c
+++ bnssrcs/command_bonus.c
@@ -20,6 +20,8 @@ static char	**get_pathlist_from_envp(char **const envp)
 	i = 0;
 	while (envp[i] != NULL && ft_strncmp(envp[i], "PATH=", 5))
 		++i;
+	if (envp[i] == NULL)
+		return (ft_split("", '\0'));
 	pathlist = ft_split(envp[i] + 5, ':');
 	return (pathlist);
 }
